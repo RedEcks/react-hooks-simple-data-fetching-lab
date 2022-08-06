a3dfc0d5afecb729277d8e612158a91bc01628d6
Xavier Dunkley <xavierxv18@gmail.com>
2022-08-06 16:56:49 -0400

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App(){
+
+    const [doggo,setdoggo]=useState([]);
+    const [isLoaded,setIsLoaded]=useState([]);
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((res)=>res.json())
+            .then((data)=>{
+                setdoggo(data.message)
+                setIsLoaded(true)
+            })
+
+    }, []);
+    
+    if(!isLoaded){ return<p>Loading...</p>}
+
+    return (<img src={doggo} alt="" height="800" width="800"/>)
+
+}
+
+export default App;
